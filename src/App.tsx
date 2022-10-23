9ec92824b2a6befb2781872e01f1dd4713cb3024
Tu Nguyen <nguyenvantu191298@gmail.com>
2022-10-23 11:22:47 +0700

    add rickroll

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,13 @@ export default function App() {
   return (
     <MetamaskProvider>
       <HelloMetamask />
+      <a
+        href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley"
+        target="_blank"
+        className="absolute bottom-10 right-10 bg-gray-500 text-white p-4 py-2 rounded pointer"
+      >
+        Source Code
+      </a>
     </MetamaskProvider>
   );
 }
